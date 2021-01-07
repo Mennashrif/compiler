2a11f32276744f0b4cccfbd54f35157ae4029aa2
menna <mennashrif2000@gmail.com>
2021-01-07 18:16:53 +0200

    add Analyser file

---
+++ TINY_COMPILER_MILSTONE1_final/TINY_COMPILER_MILSTONE1/SyntaxAnalyser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JASONParser
+{
+    public class Node
+    {
+        public List<Node> children = new List<Node>();
+        public string Name;
+        public Node(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+    class SyntaxAnalyser
+    {
+
+        int InputPointer = 0;
+        static List<Token> TokenStream;
+        public static Node root;
+        public static Node Parse(List<Token> Tokens)
+        {
+            TokenStream = Tokens;
+
+            //write your parser code
+
+            return root;
+        }
+
+        public Node match(Token_Class ExpectedToken)
+        {
+
+
+
+            return null;
+        }
+
+
+        //use this function to print the parse tree in TreeView Toolbox
+        public static TreeNode PrintParseTree(Node root)
+        {
+            TreeNode tree = new TreeNode("Parse Tree");
+            TreeNode treeRoot = PrintTree(root);
+            if (treeRoot != null)
+                tree.Nodes.Add(treeRoot);
+            return tree;
+        }
+        static TreeNode PrintTree(Node root)
+        {
+            if (root == null || root.Name == null)
+                return null;
+            TreeNode tree = new TreeNode(root.Name);
+            if (root.children.Count == 0)
+                return tree;
+            foreach (Node child in root.children)
+            {
+                if (child == null)
+                    continue;
+                tree.Nodes.Add(PrintTree(child));
+            }
+            return tree;
+        }
+    }
+}
